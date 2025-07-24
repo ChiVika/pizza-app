6ec56f0cbcdb753cb486ca474e0b7ec9db657d7a
ChiVika <animevita03@mail.ru>
2025-07-24 22:37:33 +0700

    Navigation and menu

---
+++ src/layout/Menu/Layout.tsx
@@ -0,0 +1,46 @@
+import { Link, NavLink, Outlet, useLocation } from "react-router-dom";
+import styles from './Layout.module.css';
+import Button from "../../components/Button/Button";
+import { useEffect } from "react";
+import cn from 'classnames';
+export function Layout(){
+    const location = useLocation();
+
+    useEffect(() => {
+      console.log(location);
+    }, [location])
+
+
+    return (
+      <div className={styles['layout']}>
+        <div className={styles['sidebar']}>
+          <div className={styles['user']}>
+            <img src="/person.png" alt="ava" className={styles['ava']} />
+            <div className={styles['name']}>Вика Чиняева</div>
+            <div className={styles['email']}>animevita03@mail.ru</div>
+          </div>
+          <div className={styles['menu']}>
+            <NavLink to='/' className={({isActive}) => cn(styles['link'],{
+              [styles['active']]: isActive
+            })}>
+              <img src="/menu.svg" alt="menu" />
+            Меню</NavLink>
+            <NavLink to='/cart' className={({isActive}) => cn(styles['link'], {
+              [styles['active']]: isActive
+            })}>
+            <img src="/cart.svg" alt="cart" />
+            Корзина</NavLink>
+          </div>
+          <Button className={styles['exit']}>
+            <img src="/exit.svg" alt="exit" />
+            Выход
+          </Button>
+      </div>
+      
+      <div>
+        <Outlet/>
+      </div>
+    </div>
+    );
+    
+};
